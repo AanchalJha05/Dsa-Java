f952edde561b9601c124e7ba37c83a7ba81d208a
AanchalJha05 <aanchaljha207@gmail.com>
2026-08-16 12:56:48 +0530

    practice on the topic of frequency

--- frequencies.java
+++ frequencies.java
@@ -130,6 +130,7 @@ public class frequencies{
             visited[ch]= true;
             System.out.println(ch +" -> "+ count);
         }*/
+       /* print all characters one time only
         String name = "banana";
         int[] freq = new int[26];
         for(int i=0;i<name.length();i++){
@@ -139,7 +140,162 @@ public class frequencies{
         for(int i=0;i<26;i++){
             char letter = (char)('a'+i);  // yhe (char)('a'+i) imse char jo hai vo int ko char me change krne ke liye use hota hai
             System.out.println(letter +" -> "+ freq[i]);
+        }*/
+       /* this is for maximum character in any word
+        String s ="programming";
+        int freq[] = new int[26];
+        int maxi = Integer.MIN_VALUE;
+        
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            freq[ch-'a']++;
+            
+            
+            
         }
+        for(int i=0;i<s.length();i++){
+            maxi = Math.max(maxi,freq[i]);
+        }
+        for(int i=0;i<26;i++){
+            char letter = (char)('a'+i);
+            
+            if(maxi==freq[i]){
+                System.out.println(letter + " -> "+ freq[i]);
+            }
+            
+            
+
+        }*/
+       /*  minimum numbers 
+        String s ="programming";
+        int freq[] = new int[26];
+        int mini = Integer.MAX_VALUE;
+        
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            freq[ch-'a']++;
+            
+            
+            
+        }
+        for(int i=0;i<s.length();i++){
+            if(freq[i]>0){
+                mini = Math.min(mini,freq[i]);
+            }
+            
+        }
+        for(int i=0;i<26;i++){
+            char letter = (char)('a'+i);
+            
+            if(mini==freq[i]){
+                System.out.println(letter + " -> "+ freq[i]);
+            }
+            
+            
+
+        }*/
+       /* 
+       //Find the first character with frequency 1
+        String name = "programming";
+        int freq[] = new int[26];
+        int count =0;
+        for(int i=0;i<name.length();i++){
+            char ch = name.charAt(i);
+            freq[ch-'a']++;
+            
+        }
+        for(int i=0;i<26;i++){
+            char ch = name.charAt(i);
+            if(freq[ch-'a']==1){
+                System.out.println(ch);
+                break;
+            }
+        }
+        */
+       /* first repetition character 
+        String s ="programming";
+        int freq[] = new int[26];
+        
+        
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            freq[ch-'a']++;
+            
+            
+            
+        }
+        
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            
+            
+            if(freq[ch-'a']>1){
+                System.out.println(ch + " -> "+ freq[ch-'a']);
+                break;
+            }
+            
+            
+
+        }
+        */
+       //First non-repeating character
+       /* 
+        String name ="programming";
+        int freq[] = new int[26];
+        for(int i=0;i<name.length();i++){
+            char ch = name.charAt(i);
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<name.length();i++){
+            char ch = name.charAt(i);
+            if(freq[ch-'a']==1){
+                System.out.println(ch+" -> "+ freq[ch-'a']);
+                break;
+
+            }
+        }*/
+        // Count how many distinct characters are present
+        /*String name = "programming";
+        int freq[] = new int[26];
+        int count =0;
+        for(int i = 0; i < name.length(); i++){
+            char ch = name.charAt(i);
+            freq[ch-'a']++;
+            
+        }
+        for (int i = 0; i < 26; i++) {
+            
+            if(freq[i]>0){
+                count++;
+                
+            }
+            
+        }
+        System.out.println(count);*/
+
+        // Check whether two strings have the same character frequencies
+        String n1 = "silent";
+        String n2 = "listen";
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for (int i = 0; i < n1.length(); i++) {
+            char ch = n1.charAt(i);
+            freq1[ch-'a']++;
+        }
+        for(int i=0;i<n2.length();i++){
+            char ch = n2.charAt(i);
+            freq2[ch-'a']++;
+        }
+        boolean same = true;
+        for(int i=0;i<26;i++){
+            if ((freq1[i]!= freq2[i])) {
+                same = false;
+                
+            }
+            
+        }
+        System.out.println(same);
+        
 
 
 
